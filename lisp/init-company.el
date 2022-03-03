c83649796b005e5d5ea0a165a26786810df2fd82
jfklima <jfranklincontatos@gmail.com>
2022-03-03 08:26:42 -0300

    Adicionado a capcidade de definir o company-minimun-prefix por modo.
    Agora no python-mode a var company-minimun-prefix é 1 e nos demais
    modos principais é por default 3.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,6 +1,8 @@
 (use-package company
   :demand t
-
+  :hook
+  (emacs-lisp-mode . company-mode)
+  (python-mode . company-mode)
   :init
   (add-hook 'after-init-hook 'company-tng-mode)
   (imap "TAB" 'company-complete)
@@ -14,6 +16,8 @@
 	company-selection-wrap-around t
 	company-idle-delay 0.0)
 
+  (add-hook 'python-mode-hook '(lambda () (setq-local company-minimum-prefix-length 1)))
+
   (let ((map company-active-map))
 
     (define-key map (kbd "M-l") 'company-select-previous)
@@ -24,8 +28,9 @@
 
     (define-key map (kbd "M-k") 'company-abort)
     (define-key map (kbd "C-k") 'company-abort))
+  )
 
-    (global-company-mode +1))
+    ;; (global-company-mode +1))
 
 
 (provide 'init-company)
