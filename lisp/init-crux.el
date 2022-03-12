b954255e4d3690f04aa25e0b21d22f51151d0b81
jfklima <jfranklincontatos@gmail.com>
2022-03-12 18:02:23 -0300

    Criado novos atalhos de teclado para comandos do crux.

--- lisp/init-crux.el
+++ lisp/init-crux.el
@@ -2,5 +2,13 @@
 (emap
   "a" 'crux-move-beginning-of-line)
 
+(emap
+  "C-m" 'crux-smart-open-line
+  "C-o" 'crux-smart-open-line-above
+  "C-k" 'crux-smart-kill-line
+  "C-n" 'crux-duplicate-current-line-or-region
+  "J" 'join-line
+  "C-j" 'crux-top-join-line)
+
 
 (provide 'init-crux)
