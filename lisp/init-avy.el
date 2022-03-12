cf5e6d6fb533539dcc5ddac96c7202d866756a6d
jfklima <jfranklincontatos@gmail.com>
2022-03-12 17:59:54 -0300

    Mapeados atalhos para outros comandos do avy,

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -2,6 +2,11 @@
   :config
   (setq avy-background t)
   (emap
-    "j" 'avy-goto-char))
+    "j" 'avy-goto-char)
+  (lmap
+    "j m l" 'avy-move-line
+    "j k l" 'avy-kill-whole-line
+    "j j l"  'avy-goto-line
+    "j t" 'avy-goto-char-timer))
 
 (provide 'init-avy)
